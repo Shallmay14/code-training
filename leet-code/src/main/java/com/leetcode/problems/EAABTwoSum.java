3b213db1155496975799aa63150a86e726c642fe
boying.shih@hy-tech.com.tw <boying.shih@hy-tech.com.tw>
2022-01-08 22:11:12 +0800

    EAABTwoSum

---
+++ leet-code/src/main/java/com/leetcode/problems/EAABTwoSum.java
@@ -0,0 +1,16 @@
+package com.leetcode.problems;
+
+public class EAABTwoSum {
+
+	public int[] twoSum(int[] nums, int target) {
+
+		for (var i = 0; i < nums.length; i++)
+			for (var j = i + 1; j < nums.length; j++)
+				if (target == (nums[i] + nums[j]))
+					return new int[] { i, j };
+
+		return new int[0];
+
+	}
+
+}
