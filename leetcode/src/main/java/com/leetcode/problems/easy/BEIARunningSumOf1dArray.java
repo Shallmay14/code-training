d1d34ead7ba63a4c1e14069f5c78ad63f3e3f824
boying.shih@hy-tech.com.tw <boying.shih@hy-tech.com.tw>
2023-01-17 15:27:10 +0800

    BEIARunningSumOf1dArray

--- leetcode/src/main/java/com/leetcode/problems/easy/BEIARunningSumOf1dArray.java
+++ leetcode/src/main/java/com/leetcode/problems/easy/BEIARunningSumOf1dArray.java
@@ -1,7 +1,14 @@
 package com.leetcode.problems.easy;
 
+import java.util.ArrayList;
+
 public class BEIARunningSumOf1dArray {
     public int[] runningSum(int[] nums) {
-        return nums;
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+        return sums;
     }
 }
